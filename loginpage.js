15f96993f3becf3a45a359d2e7ab82a760872230
ChirayuTrivedi <113852686+ChirayuTrivedi@users.noreply.github.com>
2024-04-19 14:46:55 +0530

    Update loginpage.js

--- loginpage.js
+++ loginpage.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 
 function App() {
   const [username, setUsername] = useState('');
@@ -31,3 +31,5 @@ function App() {
     </div>
   );
 }
+
+export default App;
